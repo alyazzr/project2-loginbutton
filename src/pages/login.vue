2ccece7e4b367c59f035ecb0232b3e35d6f7f237
alyazzr <108083113+alyazzr@users.noreply.github.com>
2023-11-30 11:56:56 +0700

    Update login.vue

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,25 +1,47 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useRouter } from 'vue-router';
-import { useAuthStore } from "c:/Users/Wildanun Nabil/project2-alya/src/stores/auth"
+import { useAuthStore } from "@/stores/auth"
+import axios from 'axios'
 
 const auth = useAuthStore()
 const username = ref('')
+const password = ref('')
 const router = useRouter()
 
-const onLogin = () => {
-    auth.login(username.value)
+const onLogin = async() => {
+  const response = await fetch('http://localhost:3000/login/', {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username: username.value,
+      password: password.value
+    })
+  })
+  
 
-    router.push('/')
+  const data = await response.json()
+  console.log(data)
+  
+  if (data.status == "success"){
+
+  }
+  else {
+    (data.status == "data yang anda masukkan tidak valid")
+  }
 }
 </script>
 
 <template>
     <div>
         <h1>Login Page</h1>
-        <div class="flex flex-col gap-2">
-            <input type="text" required v-model="username" class="border p-2" placeholder="Username"/>
-            <button @click="onLogin()" class="bg-blue-500 text-white py-1 px-3">Login</button>
-        </div>
+        <form action="" class="flex flex-col gap-2">
+            <input type="text" v-model="username" class="border p-3" placeholder="Username"/>
+            <input type="text" v-model="password" class="border p-3" placeholder="Password"/>
+            <button type="button" class="bg-blue-500 text-white py-1 px-3" @click="onLogin">Login</button>
+        </form>
     </div>
+
 </template>
